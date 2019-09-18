471bed9d5c762c9222991ed3c2d8e308186a60e8
李学斌 <lixuebin@cdeledu.com>
2019-09-18 20:20:18 +0800

    Implement modify_state for instance

--- src/models/converter_cfg.rs
+++ src/models/converter_cfg.rs
@@ -159,16 +159,6 @@ pub struct OneStepFlowSettings {
     pub target_state: Option<TargetState>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq)]
-pub struct TargetState {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(default)]
-    pub add: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(default)]
-    pub remove: Option<Vec<String>>,
-}
-
 fn is_false(val: &bool) -> bool {
     !val.clone()
 }
