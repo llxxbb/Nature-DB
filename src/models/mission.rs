f997e4c01f8289857aed532d531cb8a21baa6a9f
llxxbb <llxxbb@yeah.net>
2019-09-21 19:52:26 +0800

    fix Mission test

--- src/models/mission.rs
+++ src/models/mission.rs
@@ -129,7 +129,6 @@ impl Mission {
 
 #[cfg(test)]
 mod selector_test {
-    // TODO
     use super::*;
 
     #[test]
@@ -164,126 +163,125 @@ mod selector_test {
         let option = Mission::filter_relations(&instance, osf.clone());
         assert_eq!(option.is_some(), true);
     }
-//
-//    #[test]
-//    fn source_status_exclude() {
-//        let mut set = HashSet::<String>::new();
-//        set.insert("one".to_string());
-//        set.insert("two".to_string());
-//
-//        let mut instance = Instance::default();
-//
-//        // set state required.
-//        let osf = vec![OneStepFlow::new_for_source_status_excluded("from", "to", &set).unwrap()];
-//
-//        // condition satisfy
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_some(), true);
-//        instance.data.states = HashSet::new();
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_some(), true);
-//        instance.data.states.insert("three".to_string());
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_some(), true);
-//
-//        // condition does not satisfy
-//        instance.data.states.insert("one".to_string());
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_none(), true);
-//        instance.data.states.insert("two".to_string());
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_none(), true);
-//        instance.data.states.remove("one");
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_none(), true);
-//    }
-//
-//    #[test]
-//    fn context_needed() {
-//        let mut set = HashSet::<String>::new();
-//        set.insert("one".to_string());
-//        set.insert("two".to_string());
-//
-//        let mut instance = Instance::default();
-//
-//        // set state required.
-//        let osf = vec![OneStepFlow::new_for_context_include("from", "to", &set).unwrap()];
-//
-//        // condition does not satisfy.
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_none(), true);
-//        instance.data.context = HashMap::new();
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_none(), true);
-//        instance.data.context.insert("three".to_string(), "three".to_string());
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_none(), true);
-//        instance.data.context.insert("one".to_string(), "one".to_string());
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_none(), true);
-//
-//        // condition satisfy
-//        instance.data.context.insert("two".to_string(), "two".to_string());
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_some(), true);
-//        instance.data.context.insert("four".to_string(), "four".to_string());
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_some(), true);
-//    }
-//
-//    #[test]
-//    fn context_exclude_test() {
-//        let mut set = HashSet::<String>::new();
-//        set.insert("one".to_string());
-//        set.insert("two".to_string());
-//
-//        let mut instance = Instance::default();
-//
-//        // set state required.
-//        let osf = vec![OneStepFlow::new_for_context_excluded("from", "to", &set).unwrap()];
-//
-//        // condition satisfy
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_some(), true);
-//        instance.data.context = HashMap::new();
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_some(), true);
-//        instance.data.context.insert("three".to_string(), "three".to_string());
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_some(), true);
-//
-//        // condition does not satisfy
-//        instance.data.context.insert("one".to_string(), "one".to_string());
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_none(), true);
-//        instance.data.context.insert("two".to_string(), "two".to_string());
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_none(), true);
-//        instance.data.context.remove("one");
-//        let option = Mission::filter_relations(&instance, osf.clone());
-//        assert_eq!(option.is_none(), true);
-//    }
+
+    #[test]
+    fn source_status_exclude() {
+        let mut set = HashSet::<String>::new();
+        set.insert("one".to_string());
+        set.insert("two".to_string());
+
+        let mut instance = Instance::default();
+
+        // set state required.
+        let osf = vec![OneStepFlow::new_for_source_status_excluded("from", "to", &set).unwrap()];
+
+        // condition satisfy
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_some(), true);
+        instance.data.states = HashSet::new();
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_some(), true);
+        instance.data.states.insert("three".to_string());
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_some(), true);
+
+        // condition does not satisfy
+        instance.data.states.insert("one".to_string());
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_none(), true);
+        instance.data.states.insert("two".to_string());
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_none(), true);
+        instance.data.states.remove("one");
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_none(), true);
+    }
+
+    #[test]
+    fn context_needed() {
+        let mut set = HashSet::<String>::new();
+        set.insert("one".to_string());
+        set.insert("two".to_string());
+
+        let mut instance = Instance::default();
+
+        // set state required.
+        let osf = vec![OneStepFlow::new_for_context_include("from", "to", &set).unwrap()];
+
+        // condition does not satisfy.
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_none(), true);
+        instance.data.context = HashMap::new();
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_none(), true);
+        instance.data.context.insert("three".to_string(), "three".to_string());
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_none(), true);
+        instance.data.context.insert("one".to_string(), "one".to_string());
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_none(), true);
+
+        // condition satisfy
+        instance.data.context.insert("two".to_string(), "two".to_string());
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_some(), true);
+        instance.data.context.insert("four".to_string(), "four".to_string());
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_some(), true);
+    }
+
+    #[test]
+    fn context_exclude_test() {
+        let mut set = HashSet::<String>::new();
+        set.insert("one".to_string());
+        set.insert("two".to_string());
+
+        let mut instance = Instance::default();
+
+        // set state required.
+        let osf = vec![OneStepFlow::new_for_context_excluded("from", "to", &set).unwrap()];
+
+        // condition satisfy
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_some(), true);
+        instance.data.context = HashMap::new();
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_some(), true);
+        instance.data.context.insert("three".to_string(), "three".to_string());
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_some(), true);
+
+        // condition does not satisfy
+        instance.data.context.insert("one".to_string(), "one".to_string());
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_none(), true);
+        instance.data.context.insert("two".to_string(), "two".to_string());
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_none(), true);
+        instance.data.context.remove("one");
+        let option = Mission::filter_relations(&instance, osf.clone());
+        assert_eq!(option.is_none(), true);
+    }
 }
 
 #[cfg(test)]
 mod other_test {
-    // TODO
-//    use super::*;
-//
-//    #[test]
-//    fn input_cfg_is_empty() {
-//        let instance = Instance::default();
-//        let osf: Vec<OneStepFlow> = Vec::new();
-//        let option = Mission::filter_relations(&instance, osf);
-//        assert_eq!(option.is_none(), true)
-//    }
-//
-//    #[test]
-//    fn no_selector_but_only_executor() {
-//        let instance = Instance::default();
-//        let osf = vec![OneStepFlow::new_for_local_executor("from", "to", "local").unwrap()];
-//        let option = Mission::filter_relations(&instance, osf);
-//        assert_eq!(option.unwrap().len(), 1)
-//    }
+    use super::*;
+
+    #[test]
+    fn input_cfg_is_empty() {
+        let instance = Instance::default();
+        let osf: Vec<OneStepFlow> = Vec::new();
+        let option = Mission::filter_relations(&instance, osf);
+        assert_eq!(option.is_none(), true)
+    }
+
+    #[test]
+    fn no_selector_but_only_executor() {
+        let instance = Instance::default();
+        let osf = vec![OneStepFlow::new_for_local_executor("from", "to", "local").unwrap()];
+        let option = Mission::filter_relations(&instance, osf);
+        assert_eq!(option.unwrap().len(), 1)
+    }
 }
 
