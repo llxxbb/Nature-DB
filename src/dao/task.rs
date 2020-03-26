3efd578c54a4f6ce6910c6c87c7ff81ffd84c3c6
llxxbb <llxxbb@yeah.net>
2020-03-26 21:57:14 +0800

    - delete executed task after a given delay

--- src/dao/task.rs
+++ src/dao/task.rs
@@ -1,4 +1,5 @@
 use chrono::{Duration, Local};
+use diesel::dsl::sql;
 use diesel::prelude::*;
 use diesel::result::*;
 
@@ -36,6 +37,20 @@ impl TaskDaoImpl {
     pub fn delete(record_id: &[u8]) -> Result<usize> {
         let conn: &CONNNECTION = &CONN.lock().unwrap();
         let rtn = diesel::delete(task.filter(task_id.eq(record_id))).execute(conn);
+        match rtn {
+            Ok(num) => Ok(num),
+            Err(err) => Err(DbError::from(err)),
+        }
+    }
+
+    /// delete finished task after `delay` seconds
+    pub fn delete_finished(delay: i64) -> Result<usize> {
+        let conn: &CONNNECTION = &CONN.lock().unwrap();
+        let time = sql(&format!("'execute_time' < date_sub(now(), interval {} second)", delay));
+        let rtn = diesel::delete(task)
+            .filter(time)
+            .filter(task_state.eq(1))
+            .execute(conn);
         match rtn {
             Ok(num) => Ok(num),
             Err(err) => Err(DbError::from(err)),
@@ -119,9 +134,12 @@ impl TaskDaoImpl {
     /// increase one times and delay `delay` seconds
     pub fn increase_times_and_delay(record_id: &[u8], delay: i32) -> Result<usize> {
         let conn: &CONNNECTION = &CONN.lock().unwrap();
+        let time = Local::now().checked_add_signed(Duration::seconds(delay as i64)).unwrap().naive_local();
         let sql = format!("update task set retried_times = retried_times + 1, execute_time = datetime('now', '+{} seconds', 'localtime') where task_id = x'{}'", delay, vec_to_hex_string(&record_id));
         println!("{}", &sql);
-        match diesel::sql_query(sql)
+        match diesel::update(task)
+            .set((execute_time.eq(time), retried_times.eq(retried_times + 1)))
+            .filter(task_id.eq(record_id))
             .execute(conn) {
             Err(e) => Err(DbError::from(e)),
             Ok(num) => Ok(num)
